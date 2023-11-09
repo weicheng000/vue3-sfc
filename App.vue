b94a3566b22b474423cc8aa6afc59f9d192cddc7
jasonchiang <weicheng000@live.com>
2023-11-09 16:32:08 +0800

    first commit

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+        <counter-button></counter-button>
+        <counter-button></counter-button>
+    </div>
+</template>
+<script>
+import CounterButton from './compoents/button.vue';
+export default {
+    components:{
+        CounterButton
+    },
+    data(){
+        return{
+            msg: "hello sfc!"
+        }
+    }
+}
+</script>
---
+++ compoents/button.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <button type="button" @click="click">Counter: {{ counter }}</button>
+    </div>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            counter: 0
+        }
+    },
+    methods: {
+        click(){
+            this.counter++
+        }
+    }
+}
+</script>
